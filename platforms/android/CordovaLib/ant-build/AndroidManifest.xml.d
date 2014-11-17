f7a42cd49f54f9b34cda65670ce4a256173e6c2f
Limshad <limshad@1callgroup.com>
2014-11-17 14:56:28 +0400

    Budget

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/ant-build/AndroidManifest.xml : \
+/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/AndroidManifest.xml \
