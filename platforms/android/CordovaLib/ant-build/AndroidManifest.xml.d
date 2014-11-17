9d78e3ea4a1ef037c19e21177e9ca80a1c4fd538
Limshad <limshad@1callgroup.com>
2014-11-17 15:11:53 +0400

    test

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/ant-build/AndroidManifest.xml : \
-/Applications/MAMP/htdocs/mobile/onsenui_1.2.0/com.phonegap.sensation/platforms/android/AndroidManifest.xml \
